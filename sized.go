c192c07cc313585e3e4d83c8a402ae758c6be13a
Mark Oudsen <mark.oudsen@puzzl.nl>
2020-10-14 20:48:26 +0200

    Added dirCount functionality

--- sized.go
+++ sized.go
@@ -42,6 +42,20 @@ func DirSize(path string) (int64, error) {
     return size, err
 }
 
+func DirCount(path string) (int64, error) {
+    var count int64
+    err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() {
+            count += 1
+        }
+        return err
+    })
+    return count, err
+}
+
 func handleSizeRequest(w http.ResponseWriter, req *http.Request) {
         var dirsize int64
         var start time.Time
@@ -76,6 +90,40 @@ func handleSizeRequest(w http.ResponseWriter, req *http.Request) {
         stdlog.Printf("Directory %s has %d bytes, %f secs", dirs[0], dirsize, used.Seconds())
 }
 
+func handleCountRequest(w http.ResponseWriter, req *http.Request) {
+        var dircount int64
+        var start time.Time
+
+	// Obtain the ip address from the requestor. As this routine likely sits behind a reverse proxy,
+	// first test for an ip in the header. Only if not there, use the RemoteAddr method.
+
+	ip := req.Header.Get("X-Real-Ip")
+	if ip == "" {
+		ip = req.Header.Get("X-Forwarded-For")
+	}
+	if ip == "" {
+		ip = req.RemoteAddr
+	}
+
+        // Fetch the request directory name and process
+
+	dirs, ok := req.URL.Query()["dir"]
+
+        dircount = -1
+
+        start = time.Now()
+
+        if ok {
+            dircount,_ = DirCount(dirs[0])
+        }
+
+        used := time.Since(start)
+
+        fmt.Fprint(w,dircount)
+
+        stdlog.Printf("Directory %s has %d files, %f secs", dirs[0], dircount, used.Seconds())
+}
+
 func (service *Service) Manage() (string, error) {
 	// The deamon control section has been copied from an example of the Takama/daemon library. It's
 	// straightforward and install/using/removing the daemon is a breeze ...
@@ -105,6 +153,7 @@ func (service *Service) Manage() (string, error) {
 
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/size", handleSizeRequest)
+	http.HandleFunc("/count", handleCountRequest)
 
 	// Start the daemon in a child process. We can handle multiple requests in parallel from here.
 
